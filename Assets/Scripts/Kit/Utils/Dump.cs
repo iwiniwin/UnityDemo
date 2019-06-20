e20b72b64d16cc4091a6b080bf6006b15d434916
LensarZhang <LensarZhang@boyaa.com>
2019-06-21 00:21:32 +0800

    初步添加output

---
+++ Assets/Scripts/Kit/Utils/Dump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Output
+{
+    public static void Dump(object obj, string msg){
+        
+        if (obj is Array){
+            string output = "{";
+            Array arr = (Array)obj;
+            foreach (object a in arr)
+            {
+                output += (a + ",");
+            }
+            output += "}";
+            Debug.Log(msg + " : " + output);
+        }else{
+            Debug.Log(msg + " : " + obj);
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Output;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] a = {1, 2, 3};
+        Debug.Log("debug.bb" + " : " + a);
+        Dump(a, "bb");
+        // Debug.Log(a);
+        // Debug.Log();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
