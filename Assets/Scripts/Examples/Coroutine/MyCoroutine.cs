9602f5ec9552faff7c9b9c104512cf881c98c36a
LensarZhang <LensarZhang@boyaa.com>
2019-08-18 15:06:01 +0800

    初步模拟实现Unity StartCoroutine WatiForSeconds功能

---
+++ Assets/Scripts/Examples/Coroutine/MyCoroutine.cs
@@ -0,0 +1,93 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Example.Coroutine{
+    public class MyYieldInstruction{
+        public virtual bool IsDone(){
+            return true;
+        }
+    }
+
+    public class MyWaitForSeconds : MyYieldInstruction{
+        private float delayTime;
+        private float startTime;
+        public MyWaitForSeconds(float delayTime){
+            this.delayTime = delayTime;
+            this.startTime = -1;
+        }
+
+        public override bool IsDone(){
+            if (startTime < 0)
+                startTime = Time.time;
+            return (Time.time - startTime) >= delayTime;
+        }
+    }
+
+    public class MyCoroutine : MonoBehaviour
+    {
+        public static MyCoroutine Instance{
+            get;
+            private set;
+        }
+
+        private void Awake() {
+            if (Instance == null){
+                Instance = this;
+            }else{
+                Debug.LogError("Multi MyCoroutine.Coroutine Instance");
+            }
+        }
+
+        private List<IEnumerator> enumeratorList = new List<IEnumerator>();
+        private Dictionary<int, bool> waitForNextFrameDic = new Dictionary<int, bool>();
+
+        public MyYieldInstruction MyStartCoroutine(IEnumerator enumerator){
+            enumerator.MoveNext(); // 没有unity的实现细腻的原因
+            
+            enumeratorList.Add(enumerator);
+            if (enumerator.Current == null){
+                    waitForNextFrameDic.Add(enumeratorList.Count - 1, true);
+                }
+
+            return new MyYieldInstruction();
+        }
+
+        private void LateUpdate() {
+          
+
+            for(int i = enumeratorList.Count - 1; i >= 0; i --){
+
+                if (waitForNextFrameDic.ContainsKey(i) && waitForNextFrameDic[i]){
+                    waitForNextFrameDic[i] = false;
+                    continue;
+                }
+
+                if (enumeratorList[i].Current is MyYieldInstruction){
+                    MyYieldInstruction instruction = (MyYieldInstruction)enumeratorList[i].Current;
+                    if (!instruction.IsDone()){
+                        continue;
+                    }
+                }
+
+                
+                
+                if (!enumeratorList[i].MoveNext()){
+                    enumeratorList.RemoveAt(i);
+                    continue;
+                }
+
+                if (enumeratorList[i].Current is MyYieldInstruction){
+                    MyYieldInstruction instruction = (MyYieldInstruction)enumeratorList[i].Current;
+                    if (!instruction.IsDone()){
+                        continue;
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Examples/Coroutine/TestCoroutine.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Kit.Utils.Output;
+using Example.Coroutine;
+
+public class TestCoroutine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Dump("jjjj");
+        // MyCoroutine.Instance.StartCoroutine(new MyWaitForSeconds(3.0f));
+        StartCoroutine(Test());
+        MyCoroutine.Instance.MyStartCoroutine(MyTest());
+        // Dump("hhhh");
+    }
+
+    // IEnumerator Start(){
+    //     new WaitForSeconds(3);
+    //     Dump("start..........");
+    //     yield return StartCoroutine(MyWaitFunction(3.0f));
+    //     Dump("end ..........");
+    //     WWW www = new WWW("");
+    //     yield return www;
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dump("update...........");
+    }
+
+    IEnumerator Test(){
+        Dump(Time.time, "hhhhhhhhhhh");
+        yield return new WaitForSeconds(3);
+        Dump(Time.time, "zzz");
+        yield return null;
+        Dump(Time.time, "vvvvv");
+        yield return null;
+        Dump(Time.time, "jjj");
+        yield return new WaitForSeconds(2);
+        // yield return StartCoroutine(Test2());
+        Dump(Time.time, "bbb");
+    }
+
+    IEnumerator MyTest(){
+        Dump(Time.time, "my hhhhhhhhhhh");
+        yield return new MyWaitForSeconds(3);
+        Dump(Time.time, "my zzz");
+        yield return null;
+        Dump(Time.time, "my vvvvv");
+        yield return null;
+        Dump(Time.time, "my jjj");
+        yield return new MyWaitForSeconds(2);
+        // yield return StartCoroutine(Test2());
+        // yield return MyCoroutine.Instance.MyStartCoroutine(Test2());
+        Dump(Time.time, "my bbb");
+    }
+
+    IEnumerator Test2(){
+        yield return new WaitForSeconds(3);
+        Dump("ssss", "fff");
+    }
+}
