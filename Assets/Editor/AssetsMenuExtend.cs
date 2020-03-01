178b94f8d017c100c63f535e0697a46b1774a2cf
iwiniwin <iwiniwin@foxmail.com>
2020-03-01 21:25:38 +0800

    添加卸载无用资源菜单扩展

---
+++ Assets/Editor/AssetsMenuExtend.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+
+public class UnloadUnusedAssets
+{
+    [MenuItem("Assets/Extend/Clear")]
+    static void UnloadUnusedAssetsImmediate(){
+        EditorUtility.UnloadUnusedAssetsImmediate();
+    }
+}
