e05e661c639f14f64bae5b79227c1f7312ecedf3
iwiniwin <iwiniwin@foxmail.com>
2020-03-03 23:26:13 +0800

    Asset菜单支持构建AssetBundle

--- Assets/Editor/AssetsMenuExtend.cs
+++ Assets/Editor/AssetsMenuExtend.cs
@@ -1,9 +1,29 @@
 ﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
 
 public class UnloadUnusedAssets
 {
+    /// <summary>
+    /// 清除无用资源
+    /// </summary>
     [MenuItem("Assets/Extend/Clear")]
     static void UnloadUnusedAssetsImmediate(){
         EditorUtility.UnloadUnusedAssetsImmediate();
+    }
+
+    [MenuItem("Assets/Extend/BuildAssetBundle")]
+    static void BuildAssetBundle(){
+        string outPath = Path.Combine(Application.dataPath, "StreamingAssets");
+        if(Directory.Exists(outPath)){
+            Directory.Delete(outPath);
+        }
+        Directory.CreateDirectory(outPath);
+
+        // 构建AssetBundle
+        BuildPipeline.BuildAssetBundles(outPath, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+        
+        // 刷新
+        AssetDatabase.Refresh();
     }
 }
