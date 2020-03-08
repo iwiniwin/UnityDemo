0e39bfe3b571a28b279737d801821c3fc50c7640
iwiniwin <iwiniwin@foxmail.com>
2020-03-08 23:48:36 +0800

    添加打开Editor，Project所在文件夹扩展菜单

--- Assets/Editor/AssetsMenuExtend.cs
+++ Assets/Editor/AssetsMenuExtend.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 using System.IO;
 using UKit.Core;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using static UKit.Utils.Output;
 
-public class UnloadUnusedAssets
+public class AssetsMenuExtend
 {
     /// <summary>
     /// 清除无用资源
@@ -15,6 +17,9 @@ public class UnloadUnusedAssets
         EditorUtility.UnloadUnusedAssetsImmediate();
     }
 
+    /// <summary>
+    /// 构建 AssetBundle
+    /// </summary>
     [MenuItem("Assets/Extend/Build AssetBundle")]
     static void BuildAssetBundle(){
         string outPath = Path.Combine(Application.dataPath, "StreamingAssets");
@@ -42,6 +47,9 @@ public class UnloadUnusedAssets
         AssetDatabase.Refresh();
     }
 
+    /// <summary>
+    /// 查找重复资源
+    /// </summary>
     [MenuItem("Assets/Extend/Find Duplicate Resources")]
     static void FindDuplicateResources(){
         Dictionary<string, string> md5dic = new Dictionary<string, string>();
@@ -68,5 +76,52 @@ public class UnloadUnusedAssets
                 }
             }
         }
+    }
+
+    
+    static void Find(){
+        Dictionary<string, string> guidDics = new Dictionary<string, string>();
+        foreach (var o in Selection.objects)
+        {
+            string path = AssetDatabase.GetAssetPath(o);
+            if(!string.IsNullOrEmpty(path)){
+                string guid = AssetDatabase.AssetPathToGUID(path);
+                if(!guidDics.ContainsKey(guid)){
+                    guidDics[guid] = o.name;
+                }
+            }
+        }
+        if(guidDics.Count > 0){
+            List<string> withoutExtensions = new List<string>(){".prefab", ".unity", ".mat", ".asset"};
+            string[] files = Directory.GetFiles(Application.dataPath, "*.*", SearchOption.AllDirectories)
+                .Where(s => withoutExtensions.Contains(Path.GetExtension(s).ToLower())).ToArray();
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file = files[i];
+                if(i % 20 == 0){
+                    bool isCancel = EditorUtility.DisplayCancelableProgressBar("", file, (float)i / (float)files.Length);
+                    if(isCancel){
+                        break;
+                    }
+                }
+                foreach (KeyValuePair<string, string> guidItem in guidDics)
+                {
+                    if(Regex.IsMatch(File.ReadAllText(file), guidItem.Key)){
+                        Debug.Log(string.Format("name: {0} file: {1}", guidItem.Value, file), AssetDatabase.LoadAssetAtPath<Object>(GetRelativeAssetsPath(file)));
+                    }
+                }
+            }
+            EditorUtility.ClearProgressBar();
+        }
+    }
+
+    static string GetRelativeAssetsPath(string path){
+        return "Assets" + Path.GetFullPath(path).Replace(Path.GetFullPath(Application.dataPath), "").Replace("\\", "/");
+    }
+
+    [MenuItem("Assets/Extend/Find References", true)]  // true 验证函数，在调用具有相同 item name 的菜单函数之前调用
+    static bool VFind(){
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        return (!string.IsNullOrEmpty(path));
     }
 }
