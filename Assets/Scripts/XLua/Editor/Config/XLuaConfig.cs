ddb45649905ee4c4f6a83ae126cd826727e25dd5
iwiniwin <iwiniwin@foxmail.com>
2021-05-07 15:57:19 +0800

    修改XLua GenPath

---
+++ Assets/Scripts/XLua/Editor/Config/XLuaConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class XLuaConfig
+{
+    [CSObjectWrapEditor.GenPath]
+    public static string genPath = "Assets/3RD/XLua/Gen/";
+}
